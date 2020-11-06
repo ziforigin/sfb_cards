18592b91801a99c7b327df8c5ff0b203b8fd102b
iakovsidorov <ziforigin@gmail.com>
2020-11-06 19:45:58 +0300

    images added

--- image_combiner.py
+++ image_combiner.py
@@ -1,17 +1,17 @@
 from PIL import Image
-from os import listdir
+import os
 from os.path import isfile, join
 
 
-def image_combiner(self, name: str, images: list, card_size: tuple, margin: int, color=(255, 255, 255)):
+def image_combiner(name: str, images: list, card_size: tuple, margin: int, color=(255, 255, 255)):
     y_offset = 0
     canvas_width = card_size[0] * 3 + margin * 2
     canvas_height = card_size[1] * 3 + margin * 2
     chunk_counter = 0
-    images = list(self.chunks(images, 9))
+    images = list(chunks(images, 9))
     for big_chunk in images:
         new_image = Image.new('RGBA', (canvas_width, canvas_height), color)
-        cards_row = list(self.chunks(big_chunk, 3))
+        cards_row = list(chunks(big_chunk, 3))
         for i in range(len(cards_row)):
             for j in range(len(cards_row[i])):
                 picture = Image.open(cards_row[i][j], mode="r")
@@ -32,7 +32,7 @@ def combine(cards_list: list):
 
 
 def listfile(mypath: str) -> list:
-    onlyfiles = [file for file in listdir(mypath) if isfile(join(mypath, file))]
+    onlyfiles = [file for file in mypath if isfile(join(mypath, file))]
     return onlyfiles
 
 dirname = os.path.dirname(__file__)
